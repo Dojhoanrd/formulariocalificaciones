9de40a96da49f60da225a52342f1013cd38437e7
dojhoanrd <dojhoanrd@gmail.com>
2023-12-09 14:53:08 -0400

    Agregar archivos de proyecto.

---
+++ app14/app14/Calificacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app14
+{
+    public class Calificacion
+    {
+        public int ID { get; set; }
+        public string Profesor { get; set; }
+
+        public string Matricula { get; set; }
+        public string Nombre { get; set; }
+        public decimal Asistencia { get; set; }
+        public decimal TrabajoPractico { get; set; }
+        public decimal ExamenParcial { get; set; }
+        public decimal ExamenFinal { get; set; }
+
+        public decimal NotaFinal { get; set; }
+    }
+}
